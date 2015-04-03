c215f7e61c6ed9ba3e88a604a4eb1038dabf7d10
F483 <fabian.barkhau@gmail.com>
2015-04-03 19:07:18 +0200

    added faucet and cwpp service checks

--- checkservice.py
+++ checkservice.py
@@ -1,36 +1,59 @@
 #!/usr/bin/env python
 
-import argparse, sys, urllib2, json
+import argparse, sys, urllib2, json, httplib
+from urlparse import urlparse
 
-def check_cwpp():
-  # cwpp.chromapass.net
-  sys.exit("TODO check if cwpp is online!")
+def get_status(url):
+  parse_result = urlparse(url)
+  conn = httplib.HTTPConnection(parse_result.hostname)
+  conn.request("HEAD", parse_result.path)
+  return conn.getresponse().status
 
-def check_p2ptrade():
+def check_cwpp(url):
   try:
-    url = "http://p2ptrade.btx.udoidio.info/messages" # get from arguments ?
-    json.loads(urllib2.urlopen(url).read())
-    print "p2ptrade online!"
+    status = get_status(url)
+    if status == 502:
+      raise Exception()
+    print "cwpp service online!"
   except:
-    sys.exit("p2ptrade is offline!")
+    sys.exit("Error: faucet service offline!")
 
-def check_faucet():
-  sys.exit("TODO check if faucet is online!")
+def check_p2ptrade(url):
+  try:
+    json.loads(urllib2.urlopen(url).read())
+    print "p2ptrade service online!"
+  except:
+    sys.exit("Error: p2ptrade service offline!")
+
+def check_faucet(url):
+  try:
+    status = get_status(url)
+    if status != 200:
+      raise Exception(status)
+    print "faucet service online!"
+  except:
+    sys.exit("Error: faucet service offline!")
 
 def add_check_cwpp(subparsers):
   build_parser = subparsers.add_parser(
     'cwpp', help="Check if cwpp service is online."
   )
+  url = "http://cwpp.chromapass.net/"
+  build_parser.add_argument("--url", default=url, help="Default: %s" % url)
 
 def add_check_p2ptrade(subparsers):
   build_parser = subparsers.add_parser(
     'p2ptrade', help="Check if p2ptrade service is online."
   )
+  url = "http://p2ptrade.btx.udoidio.info/messages"
+  build_parser.add_argument("--url", default=url, help="Default: %s" % url)
 
 def add_check_faucet(subparsers):
   build_parser = subparsers.add_parser(
     'faucet', help="Check if faucet service is online."
   )
+  url = "http://chromaway.com/faucet/"
+  build_parser.add_argument("--url", default=url, help="Default: %s" % url)
 
 def get_arguments():
   parser = argparse.ArgumentParser()
